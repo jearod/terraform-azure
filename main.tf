279618ddce302abc0151f50d22826fd75953db21
Jean Rodriguez <frankrodriguez9517@gmai.com>
2025-02-14 14:14:29 -0500

    windows vm

--- main.tf
+++ main.tf
@@ -40,3 +40,28 @@ resource "azurerm_network_interface" "myNic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+
+resource "azurerm_windows_virtual_machine" "my_vm" {
+  name                  = "myVM"
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = "Standard_B1s"
+  admin_username        = "user.admin"
+  admin_password        = "admin1234!"
+
+  network_interface_ids = [
+    azurerm_network_interface.myNic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
